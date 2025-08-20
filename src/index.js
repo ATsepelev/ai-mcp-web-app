12843a8a0f98b3b8bcdb2c99fa8579d8bb01b6ae
Цепелев Алексей Александрович <aa_tsepelev@protek.ru>
2025-08-20 11:56:20 +0500

    2

---
+++ src/index.js
@@ -0,0 +1,5 @@
+export { default as ChatWidget } from './lib/ChatWidget/ChatWidget';
+export { useOpenAIChat } from './lib/useOpenAIChat';
+export { useMCPClient } from './lib/useMCPClient';
+export { useMCPServer } from './lib/useMCPServer';
+export { MCP } from './lib/mcp_core';
